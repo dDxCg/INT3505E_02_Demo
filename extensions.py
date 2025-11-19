c66e823243c60fe4445937c2499cecb8b61f3db6
Cuong Do <dDxCg@users.noreply.github.com>
2025-11-19 15:58:47 +0700

    db: redis test

--- extensions.py
+++ extensions.py
@@ -2,10 +2,13 @@
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import os
 
 cache = Cache()
 
+redis_uri = os.getenv("REDIS_URI", "redis://:<password>@redis-16478.crce185.ap-seast-1-1.ec2.cloud.redislabs.com:16478/0")
+
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["100 per hour"], 
+    storage_uri=redis_uri
 )
